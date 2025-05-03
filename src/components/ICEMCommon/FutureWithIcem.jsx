e0e60d69abddee0192d03554bccd80bc3c9beef3
Deep-mahire <Dipratnamahire2222@gmail.com>
2025-05-03 18:29:39 +0530

    push

--- src/components/ICEMCommon/FutureWithIcem.jsx
+++ src/components/ICEMCommon/FutureWithIcem.jsx
@@ -23,6 +23,7 @@ const BeFutureReady = () => {
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
+            loading="lazy"
           ></iframe>
         </div>
       </div>
